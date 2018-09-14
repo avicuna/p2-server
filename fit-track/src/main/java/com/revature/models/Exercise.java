2c9a6f515fc9940e59838d68d7951c63ee2d59bb
Kevin Pham <kpthanh01@gmail.com>
2018-09-14 19:35:18 -0400

    add feature to show All Exercise And User_Exercise

---
+++ fit-track/src/main/java/com/revature/models/Exercise.java
@@ -0,0 +1,180 @@
+package com.revature.models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@Entity
+@Table(name = "exercise")
+public class Exercise {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "exercise_id")
+	private int id;
+	private String name;
+	private String description;
+	private String type;
+	
+	@OneToMany(mappedBy="exercise")
+	@JsonIgnore
+	private List<UserExercise> userExercise;
+	
+	public Exercise() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Exercise(int id, String name, String description, String type, List<UserExercise> userExercise) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.type = type;
+		this.userExercise = userExercise;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the userExercise
+	 */
+	public List<UserExercise> getUserExercise() {
+		return userExercise;
+	}
+
+	/**
+	 * @param userExercise the userExercise to set
+	 */
+	public void setUserExercise(List<UserExercise> userExercise) {
+		this.userExercise = userExercise;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((userExercise == null) ? 0 : userExercise.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Exercise other = (Exercise) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		if (userExercise == null) {
+			if (other.userExercise != null)
+				return false;
+		} else if (!userExercise.equals(other.userExercise))
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Exercise [id=" + id + ", name=" + name + ", description=" + description + ", type=" + type
+				+ ", userExercise=" + userExercise + "]";
+	}
+
+	
+}
