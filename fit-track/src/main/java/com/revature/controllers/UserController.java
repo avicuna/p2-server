be52cbfbb68140e499934fcfd9ba22cd1af5467e
alexvicuna <vicunaalejandro@gmail.com>
2018-09-14 20:33:39 -0400

    Added Cors to User Bean

--- fit-track/src/main/java/com/revature/controllers/UserController.java
+++ fit-track/src/main/java/com/revature/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.revature.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.revature.projections.BasicUserProjection;
 import com.revature.services.UserService;
 import com.revature.services.UserWorkoutService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("users")
 public class UserController {
