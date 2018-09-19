69d711069922964fa1c5c0d24226550ce8868cf8
alexvicuna <vicunaalejandro@gmail.com>
2018-09-18 21:34:50 -0400

    Added @CrossOrigin annotation to Exercise Controller and UserExerciseController

--- fit-track/src/main/java/com/revature/controllers/ExerciseController.java
+++ fit-track/src/main/java/com/revature/controllers/ExerciseController.java
@@ -3,6 +3,7 @@ package com.revature.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.models.Exercise;
 import com.revature.services.ExerciseService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("exercise")
 public class ExerciseController {
--- fit-track/src/main/java/com/revature/controllers/UserExerciseController.java
+++ fit-track/src/main/java/com/revature/controllers/UserExerciseController.java
@@ -3,6 +3,7 @@ package com.revature.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.models.UserExercise;
 import com.revature.services.UserExerciseService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("user-exercise")
 public class UserExerciseController {
